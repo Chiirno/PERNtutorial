bb1c845ace36dc64bd54f42a072571663204ce29
Chiirno <geniuschiruno@gmail.com>
2020-09-16 23:40:39 -0300

    server works now

--- PERN-todo/server/index.js
+++ PERN-todo/server/index.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
+
+//ROUTES//
+
+//create a todo
+
+app.post("/todos", async(req,res) => {
+  try {
+    const {description} = req.body;
+    const newTodo = await pool.query("INSERT INTO todo (description)")
+  } catch (e) {
+    console.error(err.message);
+  } finally {
+
+  }
+});
+
+//get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
+
 
 app.listen(5000, () => {
   console.log("server has started on port 5000")
